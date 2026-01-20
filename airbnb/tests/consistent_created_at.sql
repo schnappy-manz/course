54b6ce11f9017c6c83144fe9700e7763438fb54c
schnappy-manz <thefoleyfamily123@gmail.com>
2026-01-20 20:13:33 +0000

    deleted assignment test - consistent_created_at

--- airbnb/tests/consistent_created_at.sql
+++
@@ -1,5 +0,0 @@
-SELECT  *
-FROM    {{ref('fct_reviews')}} fr
-        LEFT JOIN {{ref('dim_listings_cleansed')}} dlc
-        ON fr.listing_id = dlc.listing_id
-WHERE   fr.review_date < dlc.created_at
